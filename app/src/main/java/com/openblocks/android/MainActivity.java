b31f45bd24c972f7330c216b6e4d04ec909b714e
Bofa <anthonyrub@outlook.com>
2021-02-21 12:08:52 -0500

    feat: Add drawer

--- app/src/main/java/com/openblocks/android/MainActivity.java
+++ app/src/main/java/com/openblocks/android/MainActivity.java
@@ -1,7 +1,10 @@
 package com.openblocks.android;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -18,6 +21,8 @@ import com.google.android.material.tabs.TabLayout;
 import com.openblocks.android.ui.main.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +32,19 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar); //Bofa waisted 20 minutes on this hidepain
 
+        drawer = findViewById(R.id.drawer_layout);
+
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = findViewById(R.id.fab);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,5 +55,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    public void onBackPressed(){
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }

e1c2e9f37dfdb8ceb8cb402bebb07ef038306cec
Iyxan23 <nurihsanalghifari@gmail.com>
2021-02-21 10:44:28 +0700

    temp-fix: Temporary fix NPE crash

--- app/src/main/java/com/openblocks/android/MainActivity.java
+++ app/src/main/java/com/openblocks/android/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity{
                 .build();
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        // NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 }

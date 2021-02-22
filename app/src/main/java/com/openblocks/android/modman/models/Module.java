7a6843d39a6c2bd5ca1c5d1bdc67c59688e621da
Iyxan23 <nurihsanalghifari@gmail.com>
2021-02-22 15:59:53 +0700

    add: Implement Parcelable

--- app/src/main/java/com/openblocks/android/modman/models/Module.java
+++ app/src/main/java/com/openblocks/android/modman/models/Module.java
@@ -1,8 +1,11 @@
 package com.openblocks.android.modman.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.io.File;
 
-public class Module {
+public class Module implements Parcelable {
     public String name;
     public String description;
     public String classpath;
@@ -21,5 +24,41 @@ public class Module {
         this.version = version;
         this.lib_version = lib_version;
         this.jar_file = jar_file;
+    }
+
+    protected Module(Parcel in) {
+        name = in.readString();
+        description = in.readString();
+        classpath = in.readString();
+        version = in.readInt();
+        lib_version = in.readInt();
+        jar_file = new File(in.readString());
+    }
+
+    public static final Creator<Module> CREATOR = new Creator<Module>() {
+        @Override
+        public Module createFromParcel(Parcel in) {
+            return new Module(in);
+        }
+
+        @Override
+        public Module[] newArray(int size) {
+            return new Module[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(description);
+        dest.writeString(classpath);
+        dest.writeInt(version);
+        dest.writeInt(lib_version);
+        dest.writeString(jar_file.getAbsolutePath());
     }
 }

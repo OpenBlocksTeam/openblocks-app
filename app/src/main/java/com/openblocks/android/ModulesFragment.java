d114fe8a1b8aae31c060e8b90d10ea3377815117
TheClashFruit <clash.fruit123@gmail.com>
2021-02-26 17:14:15 +0100

    fix: fast fix, you never know what!

--- app/src/main/java/com/openblocks/android/ModulesFragment.java
+++ app/src/main/java/com/openblocks/android/ModulesFragment.java
@@ -59,6 +59,6 @@ public class ModulesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_projects, container, false);
+        return inflater.inflate(R.layout.fragment_modules, container, false);
     }
 }

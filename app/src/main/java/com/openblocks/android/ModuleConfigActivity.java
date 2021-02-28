7fc696d42d4cc223d3283fa84d15c9b4be088c56
Iyxan23 <nurihsanalghifari@gmail.com>
2021-02-28 17:54:41 +0700

    docs: Add more comments

--- app/src/main/java/com/openblocks/android/ModuleConfigActivity.java
+++ app/src/main/java/com/openblocks/android/ModuleConfigActivity.java
@@ -24,6 +24,9 @@ import java.util.Objects;
 
 import dalvik.system.DexClassLoader;
 
+/**
+ * This activity is where the user can edit the module's (pre-defined by the module itself) configuration
+ */
 public class ModuleConfigActivity extends AppCompatActivity {
 
     private static final String TAG = "ModuleConfigActivity";
@@ -33,8 +36,10 @@ public class ModuleConfigActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_module_config);
 
+        // We need a back button
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
+        // Get the module extra
         Module module = getIntent().getParcelableExtra("module");
 
         // Null check
@@ -48,16 +53,22 @@ public class ModuleConfigActivity extends AppCompatActivity {
         File jarfile = module.jar_file;
         String classpath = module.classpath;
 
+        // Now load the module
         try {
+            // Get the jar file's path
             final String libPath = jarfile.getAbsolutePath();
 
+            // Load it using DexClassLoader
             final DexClassLoader classloader = new DexClassLoader(libPath, getCodeCacheDir().getAbsolutePath(), null, this.getClass().getClassLoader());
             final Class<Object> module_class = (Class<Object>) classloader.loadClass(classpath);
 
+            // Then get the setupConfig method (one of the functions in OpenBlocks Module Interface)
             final Method setupConfig = module_class.getMethod("setupConfig");
 
+            // Invoke it
             OpenBlocksConfig config = (OpenBlocksConfig) setupConfig.invoke(module_class);
 
+            // Then load it
             loadConfig(config);
 
         } catch (ClassNotFoundException e) {
@@ -80,9 +91,12 @@ public class ModuleConfigActivity extends AppCompatActivity {
     }
 
     private void loadConfig(OpenBlocksConfig config) {
+        // This is where we will display the config to the recyclerview, simple
+        // Get the recyclerview
         RecyclerView rv = findViewById(R.id.module_config);
         rv.setLayoutManager(new LinearLayoutManager(this));
 
+        // And apply the adapter, done
         ConfigRecyclerViewAdapter adapter = new ConfigRecyclerViewAdapter(config.getConfigs());
         rv.setAdapter(adapter);
     }

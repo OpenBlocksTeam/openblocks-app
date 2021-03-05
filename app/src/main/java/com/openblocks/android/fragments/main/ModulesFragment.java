fd97ccb2c0a851a9e376708f3b9b49af0d01b55c
Iyxan23 <nurihsanalghifari@gmail.com>
2021-03-05 19:26:41 +0700

    refactor: Move fragments to it's own subdirectory

--- app/src/main/java/com/openblocks/android/fragments/main/ModulesFragment.java
+++ app/src/main/java/com/openblocks/android/fragments/main/ModulesFragment.java
@@ -1,9 +1,8 @@
-package com.openblocks.android;
+package com.openblocks.android.fragments.main;
 
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
@@ -11,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.openblocks.android.R;
 import com.openblocks.android.adapters.ModulesRecyclerViewAdapter;
 import com.openblocks.android.modman.models.Module;
 import com.openblocks.moduleinterface.OpenBlocksModule;
--- app/src/main/java/com/openblocks/android/fragments/main/ProjectsFragment.java
+++ app/src/main/java/com/openblocks/android/fragments/main/ProjectsFragment.java
@@ -1,4 +1,4 @@
-package com.openblocks.android;
+package com.openblocks.android.fragments.main;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.openblocks.android.R;
 
 /**
  * A simple {@link Fragment} subclass.

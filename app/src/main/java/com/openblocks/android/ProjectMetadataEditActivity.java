b89bf6f443b9d86dcc1eb5376aff31ee47766252
Iyxan23 <nurihsanalghifari@gmail.com>
2021-03-11 20:05:06 +0700

    add: Add metadata edit dialog themed activity

---
+++ app/src/main/java/com/openblocks/android/ProjectMetadataEditActivity.java
@@ -0,0 +1,14 @@
+package com.openblocks.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProjectMetadataEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_activity_project_metadata_edit);
+    }
+}

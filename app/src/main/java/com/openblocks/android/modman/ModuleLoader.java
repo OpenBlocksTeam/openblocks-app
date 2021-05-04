45bee099f0f52a4ac66eed85f7e9c6ae72379634
Iyxan23 <iyxan23@ihsan-arch-pc.localdomain>
2021-05-04 15:24:54 +0700

    fix: Fix ModuleLoader.load on loading modules

--- app/src/main/java/com/openblocks/android/modman/ModuleLoader.java
+++ app/src/main/java/com/openblocks/android/modman/ModuleLoader.java
@@ -6,31 +6,34 @@ import android.widget.Toast;
 import com.openblocks.android.modman.models.Module;
 import com.openblocks.moduleinterface.OpenBlocksModule;
 
+import dalvik.system.DexClassLoader;
+
 /**
  * ModuleLoader is a utility class used to load modules into OpenBlocksModule
  */
 public class ModuleLoader {
 
-    @SuppressWarnings("unchecked")
-    // TODO: 3/23/21 restore this
     public static <T extends OpenBlocksModule> T load(Context context, Module module, Class<T> type) {
+
+        if (module == null) {
+            Toast.makeText(context, "module is null, do you have all of the needed modules activated?", Toast.LENGTH_SHORT).show();
+            return null;
+        }
+
         ModuleManager moduleManager = ModuleManager.getInstance();
 
-        Module project_manager  = moduleManager.getActiveModule(OpenBlocksModule.Type.PROJECT_MANAGER);
-        Class<Object> project_manager_class;
-
         try {
-            project_manager_class = moduleManager.fetchModule(context, project_manager);
+            Class<Object> module_class = moduleManager.fetchModule(context, module);
 
-            return (T) project_manager_class.newInstance();
+            return type.cast(module_class.newInstance());
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
 
-            Toast.makeText(context, "Error whilst loading project manager module " + project_manager.name + ": Wrong classpath", Toast.LENGTH_LONG).show();
+            Toast.makeText(context, "Error whilst loading project manager module " + module.name + ": Wrong classpath", Toast.LENGTH_LONG).show();
         } catch (IllegalAccessException | InstantiationException e) {
             e.printStackTrace();
 
-            Toast.makeText(context, "Error while instantiating module " + project_manager.name + ": " + e.getMessage(), Toast.LENGTH_LONG).show();
+            Toast.makeText(context, "Error while instantiating module " + module.name + ": " + e.getMessage(), Toast.LENGTH_LONG).show();
         } catch (NullPointerException e) {
             e.printStackTrace();
 
